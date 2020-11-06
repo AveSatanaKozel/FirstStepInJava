00f1cc4960364c8480ce1bc7f6056cf968b1d233
AveSatanaKozel <kos1313@yandex.ru>
2020-11-06 19:26:18 +0300

    firstcommit

---
+++ RasRasRas/src/Test.java
@@ -0,0 +1,37 @@
+import java.sql.SQLOutput;
+import java.util.stream.IntStream;
+
+public class Test {
+//    public static int min(int a, int b, int c) {
+//    решение 2
+//          if (a < b)
+//              b = a;
+//          if (b < c)
+//              c = b;
+//          return c;
+//
+//
+//// решение 1
+//        int res;
+//
+//        if (a <= b && a <= c)
+//            res = a;
+//        else if (b <= a && b <= c)
+//            res = b;
+//        else
+//            res = c;
+//        return  res;
+//
+//    }
+
+    public static void main(String[] args) {
+//        System.out.println(min(1, 2, 3));
+//        System.out.println(min(-1, -2, -3));
+//        System.out.println(min(3, 5, 3));
+//        System.out.println(min(5, 5, 10));
+
+
+
+    }
+}
+
